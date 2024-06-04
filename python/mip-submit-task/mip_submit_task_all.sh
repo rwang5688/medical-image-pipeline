feeed982cf541ff363530b8124ef34ecf7a3ba29
Robert Wang <rwang5688@outlook.com>
2024-06-04 07:00:09 +0000

    add mip_submit_task_all.sh

---
+++ python/mip-submit-task/mip_submit_task_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./mip_submit_task.sh ds_292821506.json
+./mip_submit_task.sh ds_3209648408.json
+./mip_submit_task.sh ds_339833062.json
+./mip_submit_task.sh ds_3642991663.json
+./mip_submit_task.sh ds_4025360156.json
+./mip_submit_task.sh ds_571403367.json
+./mip_submit_task.sh ds_6415974217.json
+./mip_submit_task.sh ds_6451050561.json
+./mip_submit_task.sh ds_6614238035.json
+./mip_submit_task.sh ds_6670427471.json
+./mip_submit_task.sh ds_6774825273.json
+./mip_submit_task.sh ds_7255997752.json
+./mip_submit_task.sh ds_7361647728.json
+./mip_submit_task.sh ds_8155012288.json
+./mip_submit_task.sh ds_8189244869.json
+./mip_submit_task.sh ds_8548156246.json
+./mip_submit_task.sh ds_8732322741.json
+./mip_submit_task.sh ds_8834647487.json
+./mip_submit_task.sh ds_8989193730.json
+./mip_submit_task.sh ds_9189822998.json
+./mip_submit_task.sh ds_9894340694.json
+
